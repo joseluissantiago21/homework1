2ae1fa419e6182f69b92faee515e03c996566751
joseluissantiago21 <61559361+joseluissantiago21@users.noreply.github.com>
2020-02-28 14:18:08 -0600

    Create type_jluis.py

---
+++ type_jluis.py
@@ -0,0 +1,13 @@
+a.) 9-3 = 6 IS_TYPE==> <class 'int'>
+b.) 8*2.5 = 20.0 IS_TYPE==> <class 'float'>
+c.) 9/2 = 4.5 IS_TYPE==> <class 'float'>
+d.) 9/-2 = -4.5 IS_TYPE==> <class 'float'>
+e.) 9//-2 = -5 IS_TYPE==> <class 'int'>
+f.) 9%2 = 1 IS_TYPE==> <class 'int'>
+g.) 9.0%2 = 1.0 IS_TYPE==> <class 'float'>
+h.) 9%2.0 = 1.0 IS_TYPE==> <class 'float'>
+i.) 9%-2 = -1 IS_TYPE==> <class 'int'>
+j.) -9%2 = 1 IS_TYPE==> <class 'int'>
+k.) 9/-2.0 = -4.5 IS_TYPE==> <class 'float'>
+l.) 4+3*5 = 19 IS_TYPE==> <class 'int'>
+m.) (4+3)*5 = 35 IS_TYPE==> <class 'int'>
