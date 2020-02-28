6384c4daa9f88f56b9a1c40b30eb8698d4da08bd
joseluissantiago21 <61559361+joseluissantiago21@users.noreply.github.com>
2020-02-28 14:21:33 -0600

    Update type_jluis.py

--- type_jluis.py
+++ type_jluis.py
@@ -1,13 +1,14 @@
-a.) 9-3 = 6 IS_TYPE==> <class 'int'>
-b.) 8*2.5 = 20.0 IS_TYPE==> <class 'float'>
-c.) 9/2 = 4.5 IS_TYPE==> <class 'float'>
-d.) 9/-2 = -4.5 IS_TYPE==> <class 'float'>
-e.) 9//-2 = -5 IS_TYPE==> <class 'int'>
-f.) 9%2 = 1 IS_TYPE==> <class 'int'>
-g.) 9.0%2 = 1.0 IS_TYPE==> <class 'float'>
-h.) 9%2.0 = 1.0 IS_TYPE==> <class 'float'>
-i.) 9%-2 = -1 IS_TYPE==> <class 'int'>
-j.) -9%2 = 1 IS_TYPE==> <class 'int'>
-k.) 9/-2.0 = -4.5 IS_TYPE==> <class 'float'>
-l.) 4+3*5 = 19 IS_TYPE==> <class 'int'>
-m.) (4+3)*5 = 35 IS_TYPE==> <class 'int'>
+#! /usr/bin/python
+print ("a.)", "9-3 =", 9-3, "IS_TYPE==>", type(9-5))
+print ("b.)", "8*2.5 =", 8*2.5, "IS_TYPE==>", type(8*2.5))
+print ("c.)", "9/2 =", 9/2, "IS_TYPE==>", type(9/2))
+print ("d.)", "9/-2 =", 9/-2, "IS_TYPE==>", type(9/-2))
+print ("e.)", "9//-2 =", 9//-2, "IS_TYPE==>", type(9//-2))
+print ("f.)", "9%2 =", 9%2, "IS_TYPE==>", type(9%2))
+print ("g.)", "9.0%2 =", 9.0%2, "IS_TYPE==>", type(9.0%2))
+print ("h.)", "9%2.0 =", 9%2.0, "IS_TYPE==>", type(9%2.0))
+print ("i.)", "9%-2 =", 9%-2, "IS_TYPE==>", type(9%-2))
+print ("j.)", "-9%2 =", -9%2, "IS_TYPE==>", type(-9%2))
+print ("k.)", "9/-2.0 =", 9/-2.0, "IS_TYPE==>", type(9/-2.0))
+print ("l.)", "4+3*5 =", 4+3*5, "IS_TYPE==>", type(4+3*5))
+print ("m.)", "(4+3)*5 =", (4+3)*5, "IS_TYPE==>", type((4+3)*5))
